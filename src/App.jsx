e80cbb2751aaab9f8e84fd62b8f7b23ecb4f7b6d
Frank Albenesius <frankalbenesius@gmail.com>
2024-12-17 14:58:56 -0600

    It works!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
+import { useState } from "react";
 import { SweaterSVG } from "./SweaterSVG";
+import { squares } from "./state";
+
+const colors = {
+  "620 Lilac": "#a199a9",
+  "410 Cornfield": "#d78e18",
+  "240 Yell Sound Blue": "#809595",
+  "103 Sholmit": "#76746a",
+  "580 Cherry": "#641625",
+  "791 Pistachio": "#918646",
+  "478 Amber": "#b44d1f",
+  "665 Bluebell": "#526c9f",
+  "271 Flame": "#d36456",
+};
+
+const defaultState = {
+  color: "#a199a9",
+  squares: squares.map((sq) => ({
+    ...sq,
+    color: "white",
+  })),
+};
 
 function App() {
+  const [state, setState] = useState({
+    ...defaultState,
+  });
   return (
     <div className="app">
-      <div className="controls"></div>
+      <div className="controls">
+        <div className="title">amanda&#39;s sweater color picker</div>
+        <div className="color-selector">
+          {Object.keys(colors).map((color) => (
+            <button
+              key={color}
+              className="color-selector-item"
+              style={{
+                background: colors[color],
+                border: `2px dashed ${colors[color]}`,
+                borderColor:
+                  state.color === colors[color] ? "white" : colors[color],
+              }}
+              onClick={() => setState({ ...state, color: colors[color] })}
+            >
+              {color}
+            </button>
+          ))}
+        </div>
+      </div>
       <div className="sweater-container">
-        <SweaterSVG />
+        <SweaterSVG state={state} setState={setState} />
       </div>
     </div>
   );
